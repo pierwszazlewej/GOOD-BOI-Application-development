9a434b0f2970ae9a05492116bd5784d1c9867059
Alicja Kondrat <pierwszazlewej@gmail.com>
2022-02-14 23:38:58 +0100

    add leaderbord

---
+++ src/stories/LeaderboardListElement.stories.js
@@ -0,0 +1,10 @@
+import LeaderboardListElement from '../Atoms/Leaderboard/LeaderboardListElement';
+
+export default {
+  title: 'LeaderboardListElement',
+  component: LeaderboardListElement,
+};
+const Template = (args) => <LeaderboardListElement {...args} />;
+
+export const Leaderboard = Template.bind({});
+Leaderboard.args = {};
